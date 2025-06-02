31e2af021e2905f032edb8855f73eb3d281e4fab
Isaac Chargoy Vivaldo <isaac@chargoy.com>
2025-06-02 08:30:29 -0600

    more images

--- components/plataformas-equipo-analisis-esp.tsx
+++ components/plataformas-equipo-analisis-esp.tsx
@@ -6,56 +6,137 @@ export default function PlataformasEquiposAnalisisEsp() {
   return (
     <section className="relative">
       {/* Bg gradient */}
-      <div className="absolute top-0 left-0 right-0 bg-linear-to-b from-gray-800 to-gray-900 opacity-60 h-[10rem] pointer-events-none -z-10" aria-hidden="true" />
+      <div
+        className="absolute top-0 left-0 right-0 bg-linear-to-b from-gray-800 to-gray-900 opacity-60 h-[10rem] pointer-events-none -z-10"
+        aria-hidden="true"
+      />
       {/* Bg illustration */}
-      <div className="absolute top-0 left-1/2 -z-10 ml-[390px]" aria-hidden="true">
+      <div
+        className="absolute top-0 left-1/2 -z-10 ml-[390px]"
+        aria-hidden="true"
+      >
         <Image src={Illustration} className="max-w-none" alt="Illustration" />
       </div>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 font-uncut-sans mb-4">Use sensitive data without sacrificing privacy</h2>
+            <h2 className="h2 font-uncut-sans mb-4">
+              Plataformas y Equipos de Análisis Especializados
+            </h2>
             <div className="max-w-2xl mx-auto">
-              <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est.</p>
+              <p className="text-xl text-gray-400">
+                Plataformas y equipos de análisis cuantitativos y cualitativos
+                para evaluación de avance en nuestros clientes
+              </p>
             </div>
           </div>
           {/* Section content */}
-          <div className="max-w-xl mx-auto md:max-w-none flex flex-col md:flex-row md:items-center md:space-x-8 lg:space-x-16 xl:space-x-18 space-y-8 space-y-reverse md:space-y-0">
-            {/* Content */}
-            <div className="md:w-7/12 lg:w-1/2 order-1 md:order-none" data-aos="zoom-out" data-aos-delay="200">
-              <div className="text-center md:text-left">
-                <h3 className="h3 font-uncut-sans text-4xl mb-4">Standardized dev environments</h3>
-                <p className="text-xl text-gray-400 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <ul className="inline-flex flex-col text-lg text-gray-400 space-y-3">
-                  <li className="flex items-center">
-                    <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                    </svg>
-                    <span>Performance reviews</span>
-                  </li>
-                  <li className="flex items-center">
-                    <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                    </svg>
-                    <span>Objectives and goal setting</span>
-                  </li>
-                  <li className="flex items-center">
-                    <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                    </svg>
-                    <span>Manager check-ins</span>
-                  </li>
-                </ul>
+          <div className="max-w-xl mx-auto md:max-w-none grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div className="grid gap-4">
+              <div>
+                <img
+                  className="h-[24rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748826720/WhatsApp_Image_2025-05-18_at_18.39.20_1_riqbta.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="h-[18rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748826763/WhatsApp_Image_2025-05-18_at_18.39.20_3_ambcsm.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748826763/WhatsApp_Image_2025-05-18_at_18.39.21_3_nf9opi.jpg"
+                  className="h-[15rem] max-w-full rounded-lg object-cover"
+                  alt=""
+                />
               </div>
             </div>
-            {/* Image */}
-            <div className="md:w-5/12 lg:w-1/2" data-aos="zoom-out">
-              <Image className="mx-auto md:max-w-none" src={FeaturesImage} width={540} height={581} alt="Features" />
+
+            {/* --------------------------- */}
+
+            <div className="grid gap-4">
+              <div>
+                <img
+                  className="h-[14rem] max-w-full rounded-lg object-cover"
+                  alt=""
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748826763/WhatsApp_Image_2025-05-18_at_18.39.17_2_a5kwgf.jpg"
+                />
+              </div>
+              <div>
+                <img
+                  className="h-[15rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748826762/WhatsApp_Image_2025-05-18_at_18.39.17_1_akyrsl.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="h-[28rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748826763/WhatsApp_Image_2025-05-18_at_18.39.20_5_bn7oqv.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            {/* --------------------------- */}
+
+            <div className="grid gap-4">
+              <div>
+                <img
+                  className="h-[22rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748826763/WhatsApp_Image_2025-05-18_at_18.39.21_4_ip79qy.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="h-[22rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748828646/WhatsApp_Image_2025-05-18_at_18.39.17_3_aajvjp.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="h-[13em] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748828646/WhatsApp_Image_2025-05-18_at_18.39.22_2_pf0vir.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            {/* --------------------------- */}
+
+            <div className="grid gap-4">
+              <div>
+                <img
+                  className="h-[24rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748874316/10_yvrh4m.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="h-[14rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748874316/12_kfxhjf.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="h-[19rem] max-w-full rounded-lg object-cover"
+                  src="https://res.cloudinary.com/hitechrehab/image/upload/v1748874316/11_che6ve.jpg"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
